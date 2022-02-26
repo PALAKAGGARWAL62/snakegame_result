a45296419d75bdb291b1f841776bc4614ed4fd9e
palakaggarwal <palak@sopeonow.com>
2022-02-26 11:31:07 +0800

    final game

--- abir.py
+++ abir.py
@@ -3,39 +3,44 @@ import time
 import random
 
 # Define Global Variables
-delay = 0.2
-score = 0
-high_score = 0
-segments = []
+delay = 0.2  #delay
+score = 0 #score
+high_score = 0 #high_score
+segments = [] #segments
 
 #Setting up the screen, title, background color, width etc.
 # must return the window created
 def setUpScreen():
     # Set up the screen
     wn = turtle.Screen()
-    wn.title("Snake Game")
-    wn.bgcolor("grey")
+    wn.title('Snake Game')
+    wn.bgcolor('light blue')
     wn.setup(width=600, height=600)
-    wn.tracer(0) # Turns off the screen updates
+    wn.tracer(0)
+    # Turns off the screen updates
     return wn
 
 # Write score and highscore on the screen
 def trackScoreOnScreen():
     pen = turtle.Turtle()
-    pen.color("white")
+    pen.color('white')
+    #penup and hid turtle
     pen.penup()
     pen.hideturtle()
+    # Move the score to top of screen
     pen.goto(0, 260)
-    pen.write("Score: 0  High Score: 0", align="center", font=("Courier", 24, "normal"))
+    pen.write("Score: 0  High Score: 0", align= "center", font=("freestyle script", 24, "bold italic"))
     return pen
 
 # Create and return the Head of the snake
 def createSnakeHead():
     # Snake head
     head = turtle.Turtle()
+    # Set speed, shape, color and move it center of the screen
     head.speed(0)
-    head.shape("square")
-    head.color("black")
+    head.shape('square')
+    head.color('blue')
+    # Set direction as stop
     head.penup()
     head.goto(0,0)
     head.direction = "stop"
@@ -43,11 +48,12 @@ def createSnakeHead():
 
 # Create and return the first food on the screen
 def createFood():
-    # Snake food
+    # Create Snake food
     food = turtle.Turtle()
+    # Set speed, shape, color and move it some location of the screen
     food.speed(0)
-    food.shape("circle")
-    food.color("green")
+    food.shape('circle')
+    food.color('green')
     food.penup()
     food.goto(0,100)
     return food
@@ -57,6 +63,8 @@ def createFood():
 def go_up():
     if head.direction != "down":
         head.direction = "up"
+    # remove print statement after implementing this function
+    print("go_up function called")
 
 
 # Function to call when down key is pressed
@@ -64,6 +72,8 @@ def go_up():
 def go_down():
     if head.direction != "up":
         head.direction = "down"
+    # remove print statement after implementing this function
+    print("go_down function called")
 
 
 # Function to call when left key is pressed
@@ -71,43 +81,48 @@ def go_down():
 def go_left():
     if head.direction != "right":
         head.direction = "left"
-
+     # remove print statement after implementing this function
+    print("go_left function called")
 
 # Function to call when right key is pressed
 # Snake can go right only when the direction is up or down and not left
 def go_right():
     if head.direction != "left":
         head.direction = "right"
+    # remove print statement after implementing this function
+    print("go_right function called")
         
 
 # Bind Up, Down, Left and Right keys with their function
 def bindKeyboardKeys(win):
-    # Keyboard bindings
     win.listen()
-    win.onkeypress(go_up, "Up")
-    win.onkeypress(go_down, "Down")
-    win.onkeypress(go_left, "Left")
-    win.onkeypress(go_right, "Right")
+    win.onkey(go_up,'Up')
+    win.onkey(go_down,'Down')
+    win.onkey(go_right,'Right')
+    win.onkey(go_left,'Left')
+    # remove print statement after implementing this function
+    print("bindKeyboardKeys function called")
 
 # Function to call to move the snake, based on the direction of the snake
 # snake body should move automatically in that direction
 # should be called from the main loop. 
 def moveHead():
-    if head.direction == "up":
+    if head.direction=='up':
         y = head.ycor()
-        head.sety(y + 20)
-
+        head.sety(y+20)
+        
     if head.direction == "down":
         y = head.ycor()
         head.sety(y - 20)
-
-    if head.direction == "left":
-        x = head.xcor()
-        head.setx(x - 20)
-
     if head.direction == "right":
         x = head.xcor()
         head.setx(x + 20)
+    if head.direction =="left":
+        x = head.xcor()
+        head.setx(x - 20)
+    
+    # remove print statement after implementing this function
+    print("moveHead function called")
 
 # Move segments
 
@@ -128,7 +143,14 @@ def moveSegments():
         x = head.xcor()
         y = head.ycor()
         segments[0].goto(x,y)
+    
+     # Move the end segments first in reverse order
+     # Using for loop move the segments
+     # For example if there are 3 segments 2, 1, and 0
+     # Move second segment to location of first and move first segment to location of zero
 
+
+     # Move segment 0 to where the head is
 
 #Call Functions in main program
 wn = setUpScreen()
@@ -142,6 +164,8 @@ bindKeyboardKeys(wn)
 def detectCollisionWithBorder(head):
     if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
         return True
+    # remove print statement after implementing this function
+    print("detectCollisionWithBorder function called")
     
     return False
 
@@ -153,7 +177,7 @@ def handleCollisionWithBorder(head, trackScore):
     # Make head goto center of the screen
     head.goto(0,0)
     # Make head direction dummy so that it do not move
-    head.direction = "stop"
+    head.direction = 'stop'
     # Hide the segments
     for segment in segments:
         segment.goto(1000, 1000)
@@ -172,6 +196,8 @@ def handleCollisionWithBorder(head, trackScore):
 def detectCollisionWithFood(head, food):
     if head.distance(food) < 20:
         return True
+    # remove print statement after implementing this function
+    print("detectCollisionWithFood function called")
     
     return False
 
@@ -183,24 +209,24 @@ def handleCollisionWithFood(head, trackScore, food):
     global high_score
     
     # Move the food to a random spot
-    x = random.randint(-290, 290)
-    y = random.randint(-290, 290)
+    x = random.randint(-290,290)
+    y = random.randint(-290,290)
     food.goto(x,y)
 
     # Add a segment, define its shape and color
-    # append it to the segments list
-    new_segment = turtle.Turtle()
+    new_segment=turtle.Turtle()
     new_segment.speed(0)
-    new_segment.shape("square")
-    new_segment.color("blue")
+    new_segment.color('red')
+    new_segment.shape('square')
     new_segment.penup()
+     # append it to the segments list
     segments.append(new_segment)
 
     # Shorten the delay, to move snake faster
-    delay -= 0.001
+    delay-=0.001
 
     # Increase the score by 10
-    score += 10
+    score+= 10
 
     # check for high score and update it if player made a new high score
     if score > high_score:
@@ -208,7 +234,7 @@ def handleCollisionWithFood(head, trackScore, food):
     
     # Update trackScore
     trackScore.clear()
-    trackScore.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal")) 
+    trackScore.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("freestyle script", 24, "italic bold")) 
 
 
 # Detect collision of snake head with its body
@@ -219,6 +245,9 @@ def detectCollisionOfHeadWithSegment(head):
         if segment.distance(head) < 20:
             return True
         
+    # remove print statement after implementing this function
+    print("moveHead function called")
+    
     return False
 
 # Start of main game loop
--- lohith1.py
+++ lohith1.py
@@ -3,51 +3,60 @@ import time
 import random
 
 # Define Global Variables
-delay = 0.2
-score = 0
-high_score = 0
-segments = []
+delay = 0.2  #delay
+score = 0 #score
+high_score = 0 #high_score
+segments = [] #segments
+# @lohith variable name incorrect - segments(Done)
 
 #Setting up the screen, title, background color, width etc.
 # must return the window created
 def setUpScreen():
     # Set up the screen
     wn = turtle.Screen()
-    wn.title("Snake Game")
-    wn.bgcolor("grey")
-    wn.setup(width=600, height=600)
-    wn.tracer(0) # Turns off the screen updates
+    wn.title('Snake Game')
+    wn.bgcolor('light blue')
+    wn.setup(width=600, height=600) # @lohith Screen size incorrect(Done)
+    # Turns off the screen updates
+    wn.tracer(0)
+    # @lohith screen updates off missing(Done)
     return wn
 
 # Write score and highscore on the screen
 def trackScoreOnScreen():
     pen = turtle.Turtle()
-    pen.color("white")
+    pen.color('white')
+    #penup and hid turtle
     pen.penup()
     pen.hideturtle()
+    # Move the score to top of screen
     pen.goto(0, 260)
-    pen.write("Score: 0  High Score: 0", align="center", font=("Courier", 24, "normal"))
+    pen.write("Score: 0  High Score: 0", align= "center", font=("Freestyle Script", 28, "normal"))
     return pen
 
 # Create and return the Head of the snake
 def createSnakeHead():
     # Snake head
     head = turtle.Turtle()
-    head.speed(0)
-    head.shape("square")
-    head.color("black")
+    # Set speed, shape, color and move it center of the screen
+    head.speed(0) # @lohith speed should be 0(Done)
+    head.shape('square')
+    head.color('orange')
+    # @lohith penup and goto missing(Done)
     head.penup()
     head.goto(0,0)
+    # Set direction as stop
     head.direction = "stop"
     return head
 
 # Create and return the first food on the screen
 def createFood():
-    # Snake food
+    # Create Snake food
     food = turtle.Turtle()
-    food.speed(0)
-    food.shape("circle")
-    food.color("green")
+    # Set speed, shape, color and move it some location of the screen
+    food.speed(0)  # @lohith speed 0(Done)
+    food.shape('circle')
+    food.color('green')
     food.penup()
     food.goto(0,100)
     return food
@@ -57,6 +66,8 @@ def createFood():
 def go_up():
     if head.direction != "down":
         head.direction = "up"
+    # remove print statement after implementing this function
+    print("go_up function called")
 
 
 # Function to call when down key is pressed
@@ -64,6 +75,8 @@ def go_up():
 def go_down():
     if head.direction != "up":
         head.direction = "down"
+    # remove print statement after implementing this function
+    print("go_down function called")
 
 
 # Function to call when left key is pressed
@@ -71,23 +84,30 @@ def go_down():
 def go_left():
     if head.direction != "right":
         head.direction = "left"
-
+     # remove print statement after implementing this function
+    print("go_left function called")
 
 # Function to call when right key is pressed
 # Snake can go right only when the direction is up or down and not left
 def go_right():
     if head.direction != "left":
         head.direction = "right"
+    # remove print statement after implementing this function
+    print("go_right function called")
         
 
 # Bind Up, Down, Left and Right keys with their function
-def bindKeyboardKeys(win):
-    # Keyboard bindings
-    win.listen()
-    win.onkeypress(go_up, "Up")
-    win.onkeypress(go_down, "Down")
-    win.onkeypress(go_left, "Left")
-    win.onkeypress(go_right, "Right")
+def bindKeyboardKeys(wn):
+    # @lohith missing listener instance(Done)
+    
+    wn.listen()
+
+    wn.onkey(go_up,'Up')
+    wn.onkey(go_down,'Down')
+    wn.onkey(go_right,'Right')
+    wn.onkey(go_left,'Left')
+    # remove print statement after implementing this function
+    print("bindKeyboardKeys function called")
 
 # Function to call to move the snake, based on the direction of the snake
 # snake body should move automatically in that direction
@@ -96,21 +116,21 @@ def moveHead():
     if head.direction == "up":
         y = head.ycor()
         head.sety(y + 20)
-
     if head.direction == "down":
         y = head.ycor()
         head.sety(y - 20)
-
-    if head.direction == "left":
-        x = head.xcor()
-        head.setx(x - 20)
-
     if head.direction == "right":
         x = head.xcor()
         head.setx(x + 20)
+    if head.direction =="left":
+        x = head.xcor()
+        head.setx(x - 20)
+        
+    # remove print statement after implementing this function
+    print("moveHead function called")
 
 # Move segments
-
+# @lohith function name incorrect - movesegments()(Done)
 def moveSegments():
     global segments
     
@@ -118,13 +138,13 @@ def moveSegments():
      # Using for loop move the segments
      # For example if there are 3 segments 2, 1, and 0
      # Move second segment to location of first and move first segment to location of zero
-    for index in range(len(segments)-1, 0, -1):
+    for index in range(len(segments)-1, 0, -1): # @lohith var is segments
         x = segments[index-1].xcor()
         y = segments[index-1].ycor()
         segments[index].goto(x, y)
 
     # Move segment 0 to where the head is
-    if len(segments) > 0:
+    if len(segments) > 0:  # @lohith var is segments(Done)
         x = head.xcor()
         y = head.ycor()
         segments[0].goto(x,y)
@@ -155,18 +175,17 @@ def handleCollisionWithBorder(head, trackScore):
     # Make head direction dummy so that it do not move
     head.direction = "stop"
     # Hide the segments
-    for segment in segments:
+    for segment in segments:  # @lohith var is segment in segments
         segment.goto(1000, 1000)
     # Clear the segments list
-    segments.clear()
+    segments.clear()  # @lohith var is segments
     # Reset the score
     score = 0
     # Reset the delay
     delay = 0.2
     # Clear trackscore and start from 0
     trackScore.clear()
-    trackScore.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal")) 
-
+    trackScore.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Freestyle Script", 28, "normal")) 
 # Detect collision of snake head with food
 # If collision detected return True else return False
 def detectCollisionWithFood(head, food):
@@ -194,31 +213,28 @@ def handleCollisionWithFood(head, trackScore, food):
     new_segment.shape("square")
     new_segment.color("blue")
     new_segment.penup()
-    segments.append(new_segment)
-
+    segments.append(new_segment)  # @lohith var is segments(Done)
+    
     # Shorten the delay, to move snake faster
     delay -= 0.001
-
     # Increase the score by 10
     score += 10
-
     # check for high score and update it if player made a new high score
     if score > high_score:
         high_score = score
-    
-    # Update trackScore
+    # Update trackScore 
     trackScore.clear()
-    trackScore.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal")) 
-
+    trackScore.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Freestyle Script", 28, "normal")) 
 
 # Detect collision of snake head with its body
 # needs to check for all segment, thus will use for loop
 # If collision detected return True else return False
 def detectCollisionOfHeadWithSegment(head):
-    for segment in segments:
+ # @lohith not required(Done)
+    for segment in segments:  # @lohith var is segment in segments(Done)
         if segment.distance(head) < 20:
             return True
-        
+    
     return False
 
 # Start of main game loop
@@ -236,7 +252,10 @@ while True:
         
     moveSegments()
     moveHead()    
+    
+    
 
+    # This is what you are missing @Lohith
     # Detect collision of snake body with its head
     if detectCollisionOfHeadWithSegment(head):
         time.sleep(1)
@@ -244,6 +263,8 @@ while True:
         
     # Sleep for time equal to delay to add delay
     time.sleep(delay)
+    
+
 
 
 wn.mainloop()
